e3ba506a055abab369e96725417ecff07193f416
mtzfederico <fede@mtzfederico.com>
2025-02-26 21:30:27 -0500

    added encryptionKeys table

--- backend/src/db.sql
+++ backend/src/db.sql
@@ -35,6 +35,17 @@ CREATE TABLE IF NOT EXISTS authTokens (
   CONSTRAINT authTokens_userID_fk FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE
 );
 
+-- The user's age public keys. The description is some sort of text to identify the key if the user has multiple public keys
+CREATE TABLE IF NOT EXISTS encryptionKeys (
+  publicKey    VARCHAR(65)     PRIMARY KEY,
+  userID       VARCHAR(50)     NOT NULL,
+  description  VARCHAR(50)     NOT NULL,
+  createdDate  DATETIME        NOT NULL,
+  CONSTRAINT encryptionKeys_userID_fk FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE
+);
+
 -- Test User. Password is "testPassword123"
 -- INSERT INTO users (userID, email, password, roleID, createdDate) VALUES ("testUser", "test@example.com", "$2a$14$w3kWUlkLWc2wkM0FQLwiWu0.Cy05LyjaXl8xE7mIl5sB9IRDFs3Ie", "user", now());
 
+-- Test public key for "AGE-SECRET-KEY-13ZV95MTF4J8K75DR5J884E9G2FRSZNJKMRHK9TV4TF7V6TTUGETQ9MZTQ7"
+-- INSERT INTO encryptionKeys (publicKey, userID, description, createdDate) VALUES ("age1pkl3nxgdqlfe35g6x96spkvqf0ru8me2nhp5vcqeg5p5wthmuerqss6agj", "testUser", "main key", now());
