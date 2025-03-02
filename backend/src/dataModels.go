78b45416360a9b74f756d22ae801c29f563f6628
mtzfederico <fede@mtzfederico.com>
2025-03-02 17:54:00 -0500

    added GetDirectoryRequest

--- backend/src/dataModels.go
+++ backend/src/dataModels.go
@@ -10,6 +10,7 @@ type LoginRequest struct {
 	UserID   string `json:"userID"`
 	Password string `json:"password"`
 }
+
 type LogoutRequest struct {
 	UserID    string `json:"userID"`
 	AuthToken string `json:"authToken"`
@@ -21,6 +22,12 @@ type BasicRequest struct {
 }
 
 type CreateFolderRequest struct {
+	UserID    string `json:"userID"`
+	AuthToken string `json:"authToken"`
+	DirName   string `json:"dirName"`
+}
+
+type GetDirectoryRequest struct {
 	UserID    string `json:"userID"`
 	AuthToken string `json:"authToken"`
 	DirName   string `json:"dirName"`
