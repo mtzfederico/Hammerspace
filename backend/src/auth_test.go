dfc08bc95827024883ced1edf5b5333e57886fe1
mtzfederico <fede@mtzfederico.com>
2025-03-24 12:50:32 -0400

    add more tests

--- backend/src/auth_test.go
+++ backend/src/auth_test.go
@@ -8,7 +8,7 @@ import (
 // and run the tests and the benchmarks with go test -bench=.
 
 func TestIsUserIDValid(t *testing.T) {
-	items := map[string]bool{"bob": false, "1234": false, "12345": true, "myUser0": true, "sample_user": true, "john.doe": true, "noobMaster69": true, "first-last": true, "user@name": false, "user;name": false, "user,name": false, "😀": false, "aioh 😀": false, "thisUsernameIsWayTooLong": false}
+	items := map[string]bool{"bob": false, "1234": false, "12345": true, "myUser0": true, "sample_user": true, "john.doe": true, "noobMaster69": true, "first-last": true, "user@name": false, "user;name": false, "user,name": false, "😀": false, "aioh 😀": false, "thisUsernameIsWayTooLong": false, "UserName' drop table users; --": false}
 
 	for key, value := range items {
 		result := isUserIDValid(key)
@@ -25,7 +25,7 @@ func BenchmarkIsUserIDValid(b *testing.B) {
 }
 
 func TestIsEmailValid(t *testing.T) {
-	items := map[string]bool{"me@example.com": true, "hello.world@example.com": true, "defenitelyNotAnEmail.com": false, "@username": false, "": false, "noobMaster69": false, "first-last": false, "user;name": false}
+	items := map[string]bool{"me@example.com": true, "hello.world@example.com": true, "defenitelyNotAnEmail.com": false, "@username": false, "": false, "noobMaster69": false, "first-last": false, "user;name": false, "email' drop table users; --": false}
 
 	for key, value := range items {
 		result := isEmailValid(key)
